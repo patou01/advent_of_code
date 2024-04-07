88dc28b06e32797ead3c823db6194fe7852823cb
patou01 <pbobbink@gmail.com>
2024-04-07 16:04:07 +0200

    thought part 2 would be much slower, but seems this still works fine

--- 6/6.py
+++ 6/6.py
@@ -12,7 +12,7 @@ class Race:
     speed_per_charge: int = 1  # maybe part 2 won't have 1
 
     def __str__(self):
-        return f"duration: {self.duration}, record: {self.record}, beaten {self.optimize()} times"
+        return f"duration: {self.duration}, record: {self.record}"
 
     def optimize(self) -> int:
         i = 0
@@ -30,8 +30,14 @@ class Data:
     Parse the input. Basically we get 2 strings and just need to make pairs
     """
     def __init__(self, desc: List[str]):
-        times_strings = desc[0].split(":")[-1].split(" ")
-        distances_strings = desc[1].split(":")[-1].split(" ")
+        self.desc = desc
+
+        self.races_1 = self.parse_one()
+        self.race_2 = self.parse_two()
+
+    def parse_one(self):
+        times_strings = self.desc[0].split(":")[-1].split(" ")
+        distances_strings = self.desc[1].split(":")[-1].split(" ")
 
         reg_int = re.compile(r'^([\s\d]+)$')
 
@@ -48,22 +54,29 @@ class Data:
                 distances.append(int(reg.groups()[0]))
 
         assert len(times) == len(distances)
-        self.races = [Race(time, distance) for time, distance in zip(times, distances)]
+        return [Race(time, distance) for time, distance in zip(times, distances)]
+
+    def parse_two(self):
+        time = int(self.desc[0].split(":")[-1].replace(" ", ""))
+        distance = int(self.desc[1].split(":")[-1].replace(" ", ""))
+
+        return Race(time, distance)
 
 
 def part_1(data: Data):
     total = 1
-    for race in data.races:
+    for race in data.races_1:
         total *= race.optimize()
 
     print(f"Part 1: {total}")
 
 
 def part_2(data: Data):
-    pass
+    print(data.race_2)
+    print(data.race_2.optimize())
 
 
-entry = read_file(__file__, "input")
+entry = read_file(__file__, "test")
 container = Data(entry)
 part_1(container)
 part_2(container)
